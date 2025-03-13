65c8226b9cb8384e503fd6d41e8ad1fe3e3aa02e
Bruna Savelli <bnsavelli@gmail.com>
2025-03-12 23:43:33 -0300

    feat: start Profile page

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -6,9 +6,40 @@ export default function Profile() {
         <div className={styles.profile}>
             <main className={styles.main}>
                 <div className={styles.card}>
-                    
+                    <div className={styles.title}><h2>PROFILE</h2></div>
+                    <div className={styles.profile}>
+                        <div className={styles.esquerda}>
+                            <div className={styles.fotoPerfil}>
+                                <h4>Avatar Picture</h4>
+                                <img src="/perfil.png" alt="" />
+                            </div>
+                            <div className={styles.infoPerfil}>
+                                <div className={styles.accounts}>
+                                    <img src="/facebook-icon.png" alt=""style={{ width: '24px', height: '24px' }} />
+                                    <input type="text" placeholder="Add Facebook" />
+                                </div>
+                                <div className={styles.accounts}>
+                                    <img src="/x-icon.png" alt="" style={{ width: '24px', height: '24px' }}/>
+                                    <input type="text" placeholder="Add X" />
+                                </div>
+                                <div className={styles.accounts}>
+                                    <img src="/instagram-icon.png" alt="" style={{ width: '24px', height: '24px' }}/>
+                                    <input type="text" placeholder="Add Instagram" />
+                                </div>
+                                <div className={styles.accounts}>
+                                    <img src="/github-icon.png" alt="" style={{ width: '24px', height: '24px' }}/>
+                                    <input type="text" placeholder="Add Github" />
+                                </div>
+                            </div>
+                        </div>
+                        <div className={styles.direita}>
+                            <div className={styles.moreInfos}>
+                                
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </main>
         </div>
-    )
+    );
 }
